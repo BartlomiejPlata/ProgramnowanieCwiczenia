b68eb763dac7259a1c5389c45ec207425f84c768
BartlomiejPlata <bartlomiej,plata@edu.uekat.pl>
2021-10-20 16:03:40 +0200

    wprowadzenie pliku

---
+++ lab_2/lab_3.py
@@ -0,0 +1,16 @@
+print("\nzadanie 1")
+def my_function(name: str, surname: str) -> str:
+    return (f"Cześć {name} {surname}")
+
+wynik = my_function("Bartek", "Plata")
+print(wynik)
+
+print("\nzadanie 2")
+
+def mnozenie(liczba1: int, liczba2: int)->int:
+    return (liczba1*liczba2)
+
+x = mnozenie(44,12)
+print(x)
+
+print("\nzadanie 2")
